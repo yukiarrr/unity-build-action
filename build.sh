801555b6994ee0cb07b563d0a85155538c74eb17
Yuki Araikawa <mail@yukiarrr.com>
2020-02-22 01:06:19 +0900

    -logFiles to -logFile

--- build.sh
+++ build.sh
@@ -14,4 +14,4 @@ rsync -a ${script_path}/Assets/ ${PROJECT_PATH}/Assets/
 
 echo $UNITY_LICENSE_BASE64 | base64 --decode > Unity.ulf
 u3d -u $UNITY_VERSION --trace -- -batchmode -nographics -quit -silent-crashes -logFile -manualLicenseFile Unity.ulf || true
-u3d -u $UNITY_VERSION --trace -- -projectPath $PROJECT_PATH -batchmode -nographics -quit -silent-crashes -logFiles -buildTarget $BUILD_TARGET -executeMethod $EXECUTE_METHOD -outputPath $OUTPUT_PATH $COMMAND_ARGS
+u3d -u $UNITY_VERSION --trace -- -projectPath $PROJECT_PATH -batchmode -nographics -quit -silent-crashes -logFile -buildTarget $BUILD_TARGET -executeMethod $EXECUTE_METHOD -outputPath $OUTPUT_PATH $COMMAND_ARGS
