4c15aff78e52410e4a76ff9062d99944b87f3a38
Yuki Araikawa <mail@yukiarrr.com>
2020-02-22 00:53:46 +0900

    Fix log

--- build.sh
+++ build.sh
@@ -13,8 +13,5 @@ script_path=$(cd $(dirname $0); pwd)
 rsync -a ${script_path}/Assets/ ${PROJECT_PATH}/Assets/
 
 echo $UNITY_LICENSE_BASE64 | base64 --decode > Unity.ulf
-editor_log_path=${PROJECT_PATH}/editor.log
-while [ ! -e $editor_log_path ]; do sleep 0.5; done && tail -n +0 -f $editor_log_path &
-u3d -u $UNITY_VERSION -- -batchmode -nographics -quit -silent-crashes -logFile $editor_log_path -manualLicenseFile Unity.ulf || true
-u3d -u $UNITY_VERSION -- -projectPath $PROJECT_PATH -batchmode -nographics -quit -silent-crashes -logFile $editor_log_path -buildTarget $BUILD_TARGET -executeMethod $EXECUTE_METHOD -outputPath $OUTPUT_PATH $COMMAND_ARGS
-rm -f $editor_log_path
+u3d -u $UNITY_VERSION -- -batchmode -nographics -quit -silent-crashes -logFile -manualLicenseFile Unity.ulf || true
+u3d -u $UNITY_VERSION -- -projectPath $PROJECT_PATH -batchmode -nographics -quit -silent-crashes -logFiles -buildTarget $BUILD_TARGET -executeMethod $EXECUTE_METHOD -outputPath $OUTPUT_PATH $COMMAND_ARGS
