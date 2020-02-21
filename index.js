fc7d11ea322e377a98d0deb820604eaf6f53b34f
Yuki Araikawa <mail@yukiarrr.com>
2020-02-22 00:41:24 +0900

    Create async function

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
 
-process.env.UNITY_VERSION = core.getInput("unity-version");
-process.env.UNITY_LICENSE_BASE64 = core.getInput("unity-license-base64");
-process.env.BUILD_TARGET = core.getInput("build-target");
-process.env.PROJECT_PATH = core.getInput("project-path");
-process.env.EXECUTE_METHOD = core.getInput("execute-method");
-process.env.OUTPUT_PATH = core.getInput("output-path");
-process.env.COMMAND_ARGS = core.getInput("command-args");
+async function run() {
+  try {
+    process.env.UNITY_VERSION = core.getInput("unity-version");
+    process.env.UNITY_LICENSE_BASE64 = core.getInput("unity-license-base64");
+    process.env.BUILD_TARGET = core.getInput("build-target");
+    process.env.PROJECT_PATH = core.getInput("project-path");
+    process.env.EXECUTE_METHOD = core.getInput("execute-method");
+    process.env.OUTPUT_PATH = core.getInput("output-path");
+    process.env.COMMAND_ARGS = core.getInput("command-args");
 
-await exec.exec(`/bin/bash ${__dirname}/build.sh`);
+    await exec.exec(`/bin/bash ${__dirname}/build.sh`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
